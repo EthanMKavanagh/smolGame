59e27b189383c52bdc237a44db7f014dff4c874a
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-10-20 14:39:50 -0500

    styled Edit Team

--- src/components/EditTeam/EditTeam.js
+++ src/components/EditTeam/EditTeam.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {Button, TextField} from '@material-ui/core';
+import {Button, TextField, Paper, Grid} from '@material-ui/core';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+
+import './EditTeam.css';
 
 class EditTeam extends Component {
 
@@ -39,55 +41,103 @@ class EditTeam extends Component {
         return (
             <>
                 {this.props.store.team.map(team =>
-                    <div key={team.id}>
-                        <h1>Team: {team.name}</h1>
-                        <TextField
-                            id="outlined-basic"
-                            label="Team Name" 
-                            variant="outlined"
-                            type="text"
-                            placeholder={team.name}
-                            onChange={(event) => this.onChange(event, 'name')}
-                        />
-
-                        <h3>Coach: {team.coach}</h3>
-                        <TextField 
-                            id="outlined-basic"
-                            label="Coach" 
-                            variant="outlined"
-                            type="text"
-                            placeholder={team.coach}
-                            onChange={(event) => this.onChange(event, 'coach')}
-                        />
-                        
-                        <img src={team.image_url} alt='' />
-                        <TextField 
-                            id="outlined-basic"
-                            label="Image URL" 
-                            variant="outlined"
-                            type="text"
-                            placeholder={team.image_url}
-                            onChange={(event) => this.onChange(event, 'image_url')}
-                        />
-
-                        <p>Bio: {team.bio}</p>
-                        <TextField 
-                            id="outlined-basic"
-                            label="Boi" 
-                            variant="outlined"
-                            type="text"
-                            placeholder={team.bio}
-                            onChange={(event) => this.onChange(event, 'bio')}
-                        />
-
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() => this.onSave()}
+                    <Paper 
+                        key={team.id}
+                        elevation={3}
+                        className="editTeamPaper"
+                    >
+                        <Grid
+                            container
+                            direction="row"
+                            justify="center"
+                            alignItems="center"
+                            spacing={7}
                         >
-                            Save
-                        </Button>
-                    </div>
+                            <Grid 
+                                item
+                                xs={5}
+                            >
+                                <img src={team.image_url} alt='' />
+                                {/* <TextField 
+                                    id="outlined-basic"
+                                    label="Image URL" 
+                                    variant="outlined"
+                                    type="text"
+                                    placeholder={team.image_url}
+                                    onChange={(event) => this.onChange(event, 'image_url')}
+                                /> */}
+                            </Grid>
+
+                            <Grid
+                                item
+                                xs={6}
+                            >
+                                <h1>{team.name}</h1>
+                                <TextField
+                                    id="outlined-basic"
+                                    label="Team Name" 
+                                    variant="outlined"
+                                    type="text"
+                                    placeholder={team.name}
+                                    onChange={(event) => this.onChange(event, 'name')}
+                                />
+
+                                <h3>Coach: {team.coach}</h3>
+                                <TextField 
+                                    id="outlined-basic"
+                                    label="Coach" 
+                                    variant="outlined"
+                                    type="text"
+                                    placeholder={team.coach}
+                                    onChange={(event) => this.onChange(event, 'coach')}
+                                />
+
+                                <p>Bio: {team.bio}</p>
+                                <TextField 
+                                    id="outlined-basic"
+                                    label="Boi" 
+                                    variant="outlined"
+                                    type="text"
+                                    placeholder={team.bio}
+                                    onChange={(event) => this.onChange(event, 'bio')}
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid
+                            container
+                            direction="row"
+                            justify="flex-end"
+                            alignItems="flex-start"
+                        >
+                            <Grid
+                                item
+                                xs={8}
+                                className="imageFileSource"
+                            >
+                                {/* TODO!!! */}
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                >
+                                    Choose File
+                                </Button>
+                            </Grid>
+                            <Grid
+                                item
+                                xs={1}
+                                className="saveEditTeam"
+                            >
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={() => this.onSave()}
+                                >
+                                    Save
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
                 )}
             </>
         );
