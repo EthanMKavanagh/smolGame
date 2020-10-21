b3bfe9b6e3ed18c96c122c317fde6ecfa0e69176
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-10-20 19:11:07 -0500

    added styling to Edit Player Profile

--- src/components/EditPlayer/EditPlayer.js
+++ src/components/EditPlayer/EditPlayer.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {TextField, Button} from '@material-ui/core';
+import {TextField, Button, Paper, Grid} from '@material-ui/core';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+
+import './EditPlayer.css';
 
 class EditPlayer extends Component {
 
@@ -52,75 +54,180 @@ class EditPlayer extends Component {
     return (
       <>
         {this.props.store.individualPlayer.map(player =>
-          <>
-            <h1>Name: {player.name}</h1>
-            <TextField 
-              id="outlined-basic"
-              label="Name" 
-              variant="outlined"
-              type="text"
-              value={this.state.name}
-              onChange={(event) => this.onChange(event, 'name')}
-            />
-
-            <p>Age: {player.age}</p>
-            <TextField 
-              id="outlined-basic"
-              label="Age" 
-              variant="outlined"
-              type="text"
-              value={this.state.age}
-              onChange={(event) => this.onChange(event, 'age')}
-            />
-
-            <p>Number: {player.number}</p>
-            <TextField 
-              id="outlined-basic"
-              label="Number" 
-              variant="outlined"
-              type="text"
-              value={this.state.number}
-              onChange={(event) => this.onChange(event, 'number')}
-            />
-
-            <p>Position: {player.position}</p>
-            <TextField 
-              id="outlined-basic"
-              label="Position" 
-              variant="outlined"
-              type="text"
-              value={this.state.position}
-              onChange={(event) => this.onChange(event, 'position')}
-            />
-
-            <p>Height: {player.height}</p>
-            <TextField 
-              id="outlined-basic"
-              label="Height" 
-              variant="outlined"
-              type="text"
-              value={this.state.height}
-              onChange={(event) => this.onChange(event, 'height')}
-            />
-
-            <p>Weight: {player.weight}</p>
-            <TextField 
-              id="outlined-basic"
-              label="Weight" 
-              variant="outlined"
-              type="text"
-              value={this.state.weight}
-              onChange={(event) => this.onChange(event, 'weight')}
-            />
-
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={() => this.onSave(player.id)}
+          <Paper
+            elevation={3}
+            className="editPlayerPaper"
+          >
+            {/* Name Input */}
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="center"
+              className="playerName"
             >
-                Save
-            </Button>
-          </>
+              <Grid item>
+                <h1 className="nameTag">Name:</h1>
+              </Grid>
+              <Grid item>
+                <TextField 
+                  id="outlined-basic"
+                  label="Name" 
+                  variant="outlined"
+                  type="text"
+                  placeholder={player.name}
+                  onChange={(event) => this.onChange(event, 'name')}
+                />
+              </Grid>
+            </Grid>
+
+            {/* Entire Form */}
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="flex-start"
+              spacing={5}
+            >
+              <Grid item>
+                {/* Left Side of Form */}
+                <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="center"
+                  className="input"
+                >
+                  <Grid item>
+                    <p className="subtitle">Age:</p>
+                  </Grid>
+                  <Grid item>
+                    <TextField 
+                      id="outlined-basic"
+                      label="Age" 
+                      variant="outlined"
+                      type="text"
+                      placeholder={player.age}
+                      onChange={(event) => this.onChange(event, 'age')}
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="center"
+                  className="input"
+                >
+                  <Grid item>
+                    <p className="subtitle">Number:</p>
+                  </Grid>
+                  <Grid item>
+                    <TextField 
+                      id="outlined-basic"
+                      label="Number" 
+                      variant="outlined"
+                      type="text"
+                      placeholder={player.number}
+                      onChange={(event) => this.onChange(event, 'number')}
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="center"
+                  className="input"
+                >
+                  <Grid item>
+                    <p className="subtitle">Position:</p>
+                  </Grid>
+                  <Grid item>
+                    <TextField 
+                      id="outlined-basic"
+                      label="Position" 
+                      variant="outlined"
+                      type="text"
+                      placeholder={player.position}
+                      onChange={(event) => this.onChange(event, 'position')}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              {/* Right Side of Form */}
+              <Grid item>
+                <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="center"
+                  className="input"
+                >
+                  <Grid item>
+                    <p className="subtitle">Height:</p>
+                  </Grid>
+                  <Grid item>
+                    <TextField 
+                      id="outlined-basic"
+                      label="Height" 
+                      variant="outlined"
+                      type="text"
+                      placeholder={player.height}
+                      onChange={(event) => this.onChange(event, 'height')}
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="center"
+                  className="input"
+                >
+                  <Grid item>
+                    <p className="subtitle">Weight:</p>
+                  </Grid>
+                  <Grid item>
+                    <TextField 
+                      id="outlined-basic"
+                      label="Weight" 
+                      variant="outlined"
+                      type="text"
+                      placeholder={player.weight}
+                      onChange={(event) => this.onChange(event, 'weight')}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid
+              container
+              direction="row"
+              justify="flex-end"
+              alignItems="flex-start"
+            >
+              <Grid 
+                item
+                xs={1}
+                className="saveBtn"
+              >
+                {/* Save Button */}
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => this.onSave(player.id)}
+                >
+                    Save
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
         )}
       </>
     );
