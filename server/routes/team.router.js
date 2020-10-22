e2ea9e839619df623138d1ac38a51d477cd2494a
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-10-22 18:36:35 -0500

    creating team fully operational

--- server/routes/team.router.js
+++ server/routes/team.router.js
@@ -30,13 +30,45 @@ router.put('/:id', rejectUnauthenticated, rejectUser, (req, res) => {
 // POST
 router.post('/', rejectUnauthenticated, rejectUser, (req, res) => {
   let queryString = `INSERT INTO "team" ("name", "coach", "image_url", "bio")
-                    VALUES ($1, $2, $3, $4);`;
+                    VALUES ($1, $2, $3, $4) RETURNING *;`;
   pool.query(queryString, [req.body.name, req.body.coach, req.body.image_url, req.body.bio])
     .then(result => {
+      console.log('result.rows', result.rows);
+      let queryText = 'UPDATE "user" SET "team_id" = $1 WHERE "id" = $2;';
+      pool.query(queryText, [result.rows[0].id, req.user.id])
+        .then(result => {
+          res.sendStatus(201);
+        }).catch(err => {
+          res.sendStatus(500);
+          console.error('/team user POST failed', err);
+        });
       res.sendStatus(201);
     }).catch(err => {
+      res.sendStatus(500);
       console.error('/team POST failed', err);
     });
 });
+
+
+// router.post('/', rejectUnauthenticated, rejectUser, (req, res) => {
+//   const client = await pool.connect();
+//   const {name, coach, image_url, bio} = req.body;
+
+//   try {
+//     await client.query('BEGIN');
+
+//     // What goes here
+//     const firstPartQuery = 'INSERT INTO "team' ("???", "")
+//     // To here?
+
+//     await client.query('COMMIT')
+//     res.sendStatus(201)
+//   }catch(error) {
+//     await client.query('ROLLBACK')
+//     throw error
+//   }finally {
+//     client.release()
+//   }
+// })
 
 module.exports = router;
